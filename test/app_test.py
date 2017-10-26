f527b2f8316ce8d11ef17b9a238c4fbbbdb01a23
UTAGAWA Kiki <utgw@kmc.gr.jp>
2017-10-26 22:16:41 +0900

    Add register test

--- test/app_test.py
+++ test/app_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import main
 
@@ -6,12 +7,20 @@ class AppTest(unittest.TestCase):
     def setUp(self):
         main.app.testing = True
         self.client = main.app.test_client()
+        os.environ['POSTGRESQL_DB'] = 'test_utgwkk'
 
     def tearDown(self):
         pass
 
     def test_index(self):
         self.client.get('/')
+
+    def test_register(self):
+        res = self.client.post('/register', data={
+            'username': 'alice',
+            'password': 'alicealice',
+        }, follow_redirects=True)
+        self.assertIn(b'@alice', res.data)
 
 
 if __name__ == '__main__':
