8de56e0d4ea29e2f5204254df89ed72dde02b7b9
UTAGAWA Kiki <utgw@kmc.gr.jp>
2017-11-02 14:43:25 +0900

    Show following user's post on index page

--- main.py
+++ main.py
@@ -145,7 +145,22 @@ def index():
         ORDER BY p.id DESC LIMIT 8
     ''')
     posts = c.fetchall()
-    return render_template('index.html', posts=posts)
+    if logged_in()['logged_in']:
+        c.execute('''
+            SELECT p.id, p.title, p.description, u.username
+            FROM posts p
+            LEFT JOIN users u
+            ON p.user_id = u.id
+            WHERE p.user_id IN (
+                SELECT following_id FROM relations
+                WHERE follower_id = %s
+            )
+            ORDER BY p.id DESC LIMIT 8
+        ''', (session['user_id'],))
+        posts_following = c.fetchall()
+    else:
+        posts_following = []
+    return render_template('index.html', posts=posts, posts_following=posts_following)
 
 
 @app.route('/login', methods=['GET', 'POST'])
