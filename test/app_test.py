9e4f393392441353f9c3256d0daebaaf4795398e
UTAGAWA Kiki <utgw@kmc.gr.jp>
2017-11-09 14:24:07 +0900

    Add favorite test

--- test/app_test.py
+++ test/app_test.py
@@ -54,6 +54,16 @@ class AppTest(unittest.TestCase):
             'description': description
         }, follow_redirects=True)
 
+    def favorite(self, post_id):
+        return self.client.post(
+            f'/favorite/{post_id}', follow_redirects=True
+        )
+
+    def unfavorite(self, post_id):
+        return self.client.post(
+            f'/unfavorite/{post_id}', follow_redirects=True
+        )
+
     def test_index(self):
         self.client.get('/')
 
@@ -109,6 +119,38 @@ class AppTest(unittest.TestCase):
         self.register('alice', 'alicealice')
         with open('./test/data/kids_chuunibyou_girl.png', 'rb') as f:
             res = self.upload(f, 'hoge', 'fuga')
+
+    def test_favorite_post(self):
+        self.register('alice', 'alicealice')
+        with open('./test/data/kids_chuunibyou_girl.png', 'rb') as f:
+            self.upload(f, 'hoge', 'fuga')
+        self.logout()
+
+        self.register('bobby', 'bobbobbob')
+
+        # Can create a favorite
+        res = self.favorite(1)
+        self.assertIn(b'Unfavorite', res.data)
+
+        # Can remove a favorite
+        res = self.unfavorite(1)
+        self.assertIn(b'Favorite', res.data)
+
+    def test_list_favorite_post(self):
+        self.register('alice', 'alicealice')
+        for i in range(10):
+            with open('./test/data/kids_chuunibyou_girl.png', 'rb') as f:
+                self.upload(f, f'hoge{i}', f'fuga{i}')
+        self.logout()
+
+        self.register('bobby', 'bobbobbob')
+        for i in range(10):
+            self.favorite(i + 1)
+
+        res = self.client.get('/favorites')
+        for i in range(10):
+            self.assertIn(b'hoge%d' % i, res.data)
+            self.assertIn(b'fuga%d' % i, res.data)
 
 
 if __name__ == '__main__':
