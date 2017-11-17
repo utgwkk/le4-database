7f27cd1a8622bfc8cd731501f1627f99e6a644c3
UTAGAWA Kiki <utgw@kmc.gr.jp>
2017-11-17 10:21:09 +0900

    Delete description form from registration page

--- main.py
+++ main.py
@@ -235,7 +235,6 @@ def register_user():
     if request.method == 'POST':
         username = request.form.get('username', '')
         password = request.form.get('password', '')
-        description = request.form.get('description', '')
         try:
             validate_user_params(username, password)
         except ValidationError as e:
@@ -248,9 +247,9 @@ def register_user():
             try:
                 c.execute(
                     'INSERT INTO users '
-                    '(username, salt, password, description) '
-                    'VALUES (%s, %s, %s, %s) RETURNING id',
-                    (username, salt, passhash(password, salt), description)
+                    '(username, salt, password) '
+                    'VALUES (%s, %s, %s) RETURNING id',
+                    (username, salt, passhash(password, salt),)
                 )
                 lastrowid = c.fetchone()[0]
                 c.execute('''
